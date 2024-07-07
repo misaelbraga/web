9ded15845b9df5b4902274a2272e7cc18a201dfb
Misael Braga <89150191+misaelbraga@users.noreply.github.com>
2024-07-07 17:32:25 -0300

    Add vite and react

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Misael Braga</h1>
+      <p className="read-the-docs">
+        Placeholder
+      </p>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
